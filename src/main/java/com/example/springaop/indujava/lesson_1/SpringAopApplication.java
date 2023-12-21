8be2fb71ab2667c10c64568655f359a1f70d25cd
rolandoandre123 <rbardalespriale@gmail.com>
2023-12-20 20:19:47 -0500

    add : writing our first aspect Issue : DT-84

---
+++ src/main/java/com/example/springaop/indujava/lesson_1/SpringAopApplication.java
@@ -0,0 +1,27 @@
+package com.example.springaop.indujava.lesson_1;
+
+import com.example.springaop.indujava.lesson_1.service.ShapeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan({"com.example.springaop.indujava.lesson_1"})
+@SpringBootApplication
+public class SpringAopApplication implements CommandLineRunner {
+
+  @Autowired
+  private ShapeService shapeService;
+
+  public static void main(String... args) {
+    SpringApplication.run(SpringAopApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    String name = this.shapeService.getTriangle().getName();
+    System.out.println("name = " + name);
+  }
+
+}
---
+++ src/main/java/com/example/springaop/indujava/lesson_1/service/ShapeService.java
@@ -0,0 +1,20 @@
+package com.example.springaop.indujava.lesson_1.service;
+
+import com.example.springaop.indujava.lesson_1.model.Circle;
+import com.example.springaop.indujava.lesson_1.model.Triangle;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@Service
+public class ShapeService {
+
+  @Autowired
+  private Circle circle;
+  @Autowired
+  private Triangle triangle;
+
+}
